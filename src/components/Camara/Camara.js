1b66f1525d57aa2bca5b0d6f2ce27b1ea5b6a1b2
Alejandro Lavado <alejandro.lvd@gmail.com>
2020-10-01 21:54:36 -0300

    ???

--- src/components/Camara/Camara.js
+++ src/components/Camara/Camara.js
@@ -11,8 +11,8 @@ const Camara = props => {
       <Webcam
         videoConstraints={{
           aspectRatio: (window.screen.width / (window.screen.height - 55)),
-          width: { min: window.screen.width, max: window.screen.width },
-          height: { min: window.screen.height - 55, max: window.screen.height - 55},
+          height: { min: window.screen.width, max: window.screen.width },
+          width: { min: window.screen.height - 55, max: window.screen.height - 55},
           facingMode: 'environment'
         }}
         ref={webcamRef}
